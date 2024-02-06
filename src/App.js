4a0af345b3e91ac474e89e25dff7ad400ff01b3a
KaramonPiotr <piotrekk1020@gmail.com>
2024-02-06 20:44:07 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
+import logo1 from './logo.jpg';
+import logo2 from './logo2.png';
+import logo3 from './logo3.gif'
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [currentImage, setCurrentImage] = useState(logo1);
+  const [currentText, setCurrentText] = useState("Czy zostaniesz moją walentynką ???");
+  const [buttonSize, setButtonSize] = useState(100); 
+  const [fontSize, setFontSize] = useState(16); 
+
+  const handleNoClick = () => {
+    setCurrentImage(logo2);
+    setCurrentText("Może jednak???");
+    setButtonSize(buttonSize + 10); 
+    setFontSize(fontSize + 2); 
+  };
+
+  const handleYesClick = () => {
+    setCurrentImage(logo3);
+    setCurrentText("Happi Happi Happi");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{currentText}</h1>
+        <img src={currentImage} className="App-logo" alt="logo" />
+        <div className="button-container">
+          <button className="button-yes" style={{ width: buttonSize, height: buttonSize, fontSize: `${fontSize}px` }} onClick={handleYesClick}>Tak 😍</button>
+          <button className="button-no" onClick={handleNoClick}>Nie 😭</button>
+        </div>
       </header>
     </div>
   );
